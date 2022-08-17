b4bfe6d0b3cf02ccc00565fca746aa596b367943
Cristian Ordonez <cristianordonezrd@gmail.com>
2022-08-16 22:44:02 -0400

    added initial connection to mongoDB

---
+++ server/database/db.ts
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const mongoDB = 'mongodb://127.0.0.1/blueOceanCapstoneDB';
+
+mongoose.connect(mongoDB);
+
+//Get the default connection
+const db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+export default db;
