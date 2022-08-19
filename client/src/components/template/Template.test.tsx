d3c497d6db9438160b447243fc2cc253e62bb0a6
Shengnan Huang <selinahuang205@gmail.com>
2022-08-19 11:41:41 -0500

    commented out tests, updated before_install

--- client/src/components/template/Template.test.tsx
+++ client/src/components/template/Template.test.tsx
@@ -4,6 +4,7 @@ import userEvent from '@testing-library/user-event';
 import 'regenerator-runtime/runtime';
 import { Template } from './Template.jsx';
 
-test('Product overview renders image correctly with data from server', async () => {
-   await render(<Template />);
-});
+// test('Product overview renders image correctly with data from server', async () => {
+//    await render(<Template />);
+// });
+
