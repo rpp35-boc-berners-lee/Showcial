4354cc1894efed698a11c6617cb5dcec2cabbf27
Xiuqi Cai <cauxiuqi2013@gmail.com>
2022-08-22 23:16:27 -0700

    Added template for header

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { Template } from './template/Template';
+import { Header } from './Header/Header';
 
 export const App = () => {
    return (
-      <>
+      <div>
+         <Header />
          <Routes>
             <Route path='/' element={<Template />} />
          </Routes>
-      </>
+      </div>
    );
 };
---
+++ client/src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.scss';
+
+export const Header = () => {
+   return (
+      <div>
+         <p>This is Header</p>
+      </div>
+   );
+};
