eee7c5d53c1e39d43c6ab53e334203a9d08c525f
Kevin On <idealistkevin@gmail.com>
2022-08-25 23:49:22 -0700

    Removed MUI Accordion and added custom accordion

--- client/src/components/homepage/homepage_components/YourWatchList.tsx
+++ client/src/components/homepage/homepage_components/YourWatchList.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect }  from 'react';
 import './YourWatchList.scss';
 import { VideoCard } from './VideoCard';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { set } from 'cypress/types/lodash';
 
 type ChildProps = {
   watchList: any;
@@ -21,20 +22,34 @@ type Video = {
   id: number;
 }
 
+
+
 export const YourWatchList:React.FC<ChildProps> = ({ watchList, config }) => {
+  const [displayedVideos, setDisplayedVideos] = useState([]);
+  const [numDisplayed, setNumDisplayed] = useState(0);
+  const [maxRowCards, setMaxRowCards] = useState(0);
+
+  useEffect(() => {
+    let box = document.querySelector('.MuiGrid-container');
+    // let card = document.querySelector('.MuiCard-root');
+    if (box !== null) {
+      let maxWidthCards = Math.floor(box.clientWidth / 300);
+      setMaxRowCards(maxWidthCards);
+      setNumDisplayed(numDisplayed + maxWidthCards);
+      setDisplayedVideos(watchList.slice(0, maxWidthCards))
+    }
+  }, []);
+
+  useEffect(() => {
+    setDisplayedVideos(watchList.slice(0, numDisplayed - 1));
+  }, [numDisplayed]);
+
    return (
-    <Accordion defaultExpanded={true}>
-      <AccordionSummary
-        expandIcon={<ExpandMoreIcon />}
-        aria-controls='watch_list'
-        id='watch_list'
-      >
-        <Typography>YOUR WATCH LIST</Typography>
-      </AccordionSummary>
-      <AccordionDetails>
+    <div>
+      <Typography>YOUR WATCH LIST</Typography>
       <Box sx={{ width: '100%' }}>
-        <Grid container spacing={4}>
-          {watchList.map((video: Video) => {
+        <Grid container spacing={4} justifyContent='center'>
+          {displayedVideos.map((video: Video) => {
             return (
               <Grid item xs={0} key={video.id}>
                 <VideoCard base_url={config.images.base_url}
@@ -47,7 +62,13 @@ export const YourWatchList:React.FC<ChildProps> = ({ watchList, config }) => {
           })}
         </Grid>
       </Box>
-      </AccordionDetails>
-    </Accordion>
+      <Stack spacing={2} direction="row">
+        {displayedVideos.length < watchList.length ?
+          <Button variant="text" startIcon={<ExpandMoreIcon />} onClick={() => setNumDisplayed(numDisplayed + maxRowCards - 1)}>SHOW MORE</Button> : null}
+        {displayedVideos.length > maxRowCards ?
+          <Button variant="text" startIcon={<ExpandLessIcon />} onClick={() =>
+            setNumDisplayed((numDisplayed - maxRowCards - 1 < maxRowCards) ? maxRowCards : (numDisplayed - maxRowCards - 1))}>SHOW LESS</Button> : null}
+      </Stack>
+    </div>
    );
 };
