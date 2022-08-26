4d4037618f34acef254a926cd8f1ff28b65844a5
Josh Waalkes <josh.waalkes21@gmail.com>
2022-08-25 21:42:01 -0600

    working on getting passport to authenticate

---
+++ server/authentication/auth.ts
@@ -0,0 +1,46 @@
+import { RequestPage } from '@mui/icons-material';
+import { Request, Response, Router } from 'express';
+const bcrypt = require('bcrypt');
+const router = Router();
+
+const saltRounds = 10;
+
+
+// router.get('/configuration', async (req: Request, res: Response) => {
+//   let params = req.params as unknown as Params;
+//   res.send(await tmdb.getConfig());
+// })
+type Params = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  services: string[];
+
+};
+
+router.get('/signup', (req: Request, res: Response) => {
+  let params = req.params as unknown as Params;
+  let firstName = params.firstName;
+  let lastName = params.lastName;
+  let email = params.email;
+  let password = params.password;
+  let services = params.services;
+  //find one from db using email, if unsuccessful, hash password and store a new user
+    //if successful, res send 'email already in use, sign in or try another'
+
+  bcrypt.hash(password, saltRounds, (hash) => {
+    //store user data here
+  })
+});
+
+router.get('/signin', (req: Request, res: Response) => {
+  let params = req.params as unknown as Params;
+  let email = params.email;
+  let password = params.password;
+  //search db for email,
+    //if successful, compare password against hashed password using bcryt.compare
+      //if true, update session
+      //if false, res.send('wrong email or password')
+    //if unsuccessful, res.send('wrong email or password')
+})
--- server/server.ts
+++ server/server.ts
@@ -7,21 +7,55 @@ import api_router from './api/routes/tmdb_api';
 import { router as baseEndpointRoute } from './routes/base-endpoint.route';
 import db from './database/db';
 var session = require('express-session');
-require('dotenv').config();
+const MongoStore = require('connect-mongo');
+var passport = require('passport');
+var LocalStrategy = require('passport-local');
+const bcrypt = require('bcrypt');
+
+
+
 
 
 const port = 8080;
+const db_conn = process.env.DB_CONN
+const secret = process.env.SESSION_SECRET
 
 const app = express();
 //MIDDLEWARE
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  cookie: {}
-}))
 
 app.use(compression());
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.use(session({
+  secret: secret,
+  cookie: {
+     path: '/home',
+     httpOnly: true,
+     secure: false,
+     maxAge: 300000
+  },
+  resave: false,
+  saveUninitialized: false,
+store: MongoStore.create({mongoUrl: db_conn})
+}))
+
+passport.use(new LocalStrategy(function verify(username, password, cb) {
+   ///use same pattern but with mongoose and bcrpyt
+
+   // db.get('SELECT * FROM users WHERE username = ?', [ username ], function(err, row) {
+   //   if (err) { return cb(err); }
+   //   if (!row) { return cb(null, false, { message: 'Incorrect username or password.' }); }
+
+   //   crypto.pbkdf2(password, row.salt, 310000, 32, 'sha256', function(err, hashedPassword) {
+   //     if (err) { return cb(err); }
+   //     if (!crypto.timingSafeEqual(row.hashed_password, hashedPassword)) {
+   //       return cb(null, false, { message: 'Incorrect username or password.' });
+   //     }
+   //     return cb(null, row);
+   //   });
+   // });
+ }));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
