9fe793f3af5968d0c841d37809b16f9943a25253
Xiuqi Cai <cauxiuqi2013@gmail.com>
2022-08-26 21:17:27 -0700

    Implemented toggle between following and for you

--- client/src/components/personal-profile/index.tsx
+++ client/src/components/personal-profile/index.tsx
@@ -1,19 +1,39 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-// import { Route, Switch } from "react-router";
 import './PersonalProfile.scss';
-import { SelectBar } from './select-bar/SelectBar';
+// import { SelectBar } from './select-bar/SelectBar';
 import { ForYou } from './for-you/ForYou';
 import { FollowingList } from './following-list/FollowingList';
 
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import GroupOutlinedIcon from '@mui/icons-material/GroupOutlined';
+import AssistantPhotoOutlinedIcon from '@mui/icons-material/AssistantPhotoOutlined';
+
+
 export const PersonalProfile = () => {
+  const [value, setValue] = React.useState(1);
+  let currentOption = value;
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    console.log('value: ',value);
+    setValue(newValue);
+    console.log('new value: ',  newValue);
+    currentOption = newValue
+  };
+  
+  const SelectBar = () => {
+    return (
+      <Tabs value={value} onChange={handleChange} aria-label="icon label tabs example" centered>
+        <Tab icon={<GroupOutlinedIcon />} label="FOLLOWING" />
+        <Tab icon={<AssistantPhotoOutlinedIcon />} label="FOR YOU" />
+      </Tabs>
+    );
+  }
+  
    return (
       <div>
         <SelectBar />
-        <Routes>
-          <Route path='/' element={ <ForYou /> } />
-          <Route path='following' element={ <FollowingList /> } />
-        </Routes>
+        {currentOption === 1 ? <ForYou /> : <FollowingList />}
       </div>
    );
 };
