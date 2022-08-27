4449363145804ca14822ad2ab2b9eb80df1f0e38
Xiuqi Cai <cauxiuqi2013@gmail.com>
2022-08-27 10:36:41 -0700

    Added template

--- client/src/components/personal-profile/following-list/FollowingList.tsx
+++ client/src/components/personal-profile/following-list/FollowingList.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
 import './FollowingList.scss';
+import { FollowingItem } from '../following-item/FollowingItem';
 
-export const FollowingList = () => {
+import { Box, Paper, Grid } from '@mui/material';
+
+
+// export const FollowingList = () => {
+//    return (
+//       <div>
+//          <p>This is following list</p>
+//       </div>
+//    );
+// };
+
+
+
+ export const FollowingList = () => {
    return (
-      <div>
-         <p>This is following list</p>
-      </div>
+     <Box sx={{ flexGrow: 1 }}>
+       <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+         {Array.from(Array(6)).map((_, index) => (
+           <Grid xs={2} sm={4} md={4} key={index}>
+             <FollowingItem />
+           </Grid>
+         ))}
+       </Grid>
+     </Box>
    );
-};
+ }
--- client/src/components/personal-profile/watch-providers/WatchProviders.tsx
+++ client/src/components/personal-profile/watch-providers/WatchProviders.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './WatchProviders.scss';
-import { styled } from '@mui/material/styles';
-import Chip from '@mui/material/Chip';
-import Paper from '@mui/material/Paper';
-import TagFacesIcon from '@mui/icons-material/TagFaces';
+import { styled, Chip, Paper, Container, Divider } from '@mui/material';
+// import Chip from '@mui/material/Chip';
+// import Paper from '@mui/material/Paper';
+import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
+import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
 
-interface ChipData {
+interface Services {
   key: number;
   label: string;
 }
@@ -15,22 +16,64 @@ const ListItem = styled('li')(({ theme }) => ({
 }));
 
 export const WatchProviders = () => {
-  const [chipData, setChipData] = React.useState<readonly ChipData[]>([
+  // Todo: retrieve user's owned services from the database to set up the initial state here
+  
+  const [ownedServices, setOwnedServices] = React.useState([
     { key: 0, label: 'Netflix' },
     { key: 1, label: 'Hulu' },
     { key: 2, label: 'Disney+' },
+  ]);
+  
+  const [unownedServices, setUnownedServices] = React.useState([
     { key: 3, label: 'HBO Max' },
     { key: 4, label: 'Prime Video' },
   ]);
+  
 
-  const handleDelete = (chipToDelete: ChipData) => () => {
-    console.log('chipToDelete: ', chipToDelete);
-    setChipData((chips) => chips.filter((chip) => chip.key !== chipToDelete.key));
-    console.log(chipData);
+  const handleDelete = (servicesToDelete: Services) => () => {
+    console.log('servicesToDelete: ', servicesToDelete);
+    setOwnedServices((services) => services.filter((service) => service.key !== servicesToDelete.key));
+    console.log(ownedServices);
   };
-
-  return (
-    <Paper
+  
+  const OwnedProviders = () => {
+    return (
+      <Container
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          flexWrap: 'wrap',
+          listStyle: 'none',
+          p: 0.5,
+          m: 0,
+        }}
+        component="ul"
+    >
+      {ownedServices.map((service) => {
+        console.log('Owned Providers: ', service);
+        return (
+          <ListItem key={service.key}>
+            <Chip
+              label={service.label}
+              onDelete={handleDelete(service)}
+              deleteIcon={<RemoveCircleOutlineIcon />}
+            />
+          </ListItem>
+        );
+      })}
+    </Container>
+    )
+  }
+  
+  
+  
+  const handleAdd = (serviceToAdd: any) => {
+    
+  }
+  
+  const UnownedProviders = () => {
+    return (
+    <Container
       sx={{
         display: 'flex',
         justifyContent: 'center',
@@ -41,17 +84,28 @@ export const WatchProviders = () => {
       }}
       component="ul"
     >
-      {chipData.map((data) => {
-        console.log(data);
+      {unownedServices.map((service) => {
+        console.log('Unowned Providers: ', service);
         return (
-          <ListItem key={data.key}>
+          <ListItem key={service.key}>
             <Chip
-              label={data.label}
-              onDelete={handleDelete(data)}
+              label={service.label}
+              onDelete={handleDelete(service)}
+              deleteIcon={<AddCircleOutlineOutlinedIcon />}
             />
           </ListItem>
         );
       })}
+    </Container>
+    )
+  }
+
+  return (
+    <Paper>
+      <OwnedProviders />
+      <Divider variant="middle"/>
+      <UnownedProviders />
     </Paper>
+    
   );
 };
