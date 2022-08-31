e4bdf71f18c8aaa35c5ee00e8cf1816d9a4472b2
Kevin On <idealistkevin@gmail.com>
2022-08-30 18:31:33 -0700

    Working through search page/functionality

--- client/src/components/search/Search.tsx
+++ client/src/components/search/Search.tsx
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { ConfigAPI, APIResponse  } from '../../../../types';
-import { VideoCard } from '../shared/VideoCard'
+import { YourWatchList } from '../../components/homepage/homepage_components/YourWatchList';
 
 type ChildProps = {
   query: string;
-  config: any;
 }
 
-export const Search:React.FC<ChildProps> = ({ query, config }) => {
+export const Search:React.FC<ChildProps> = ({ query = '' }) => {
+  const [config, setConfig] = useState<ConfigAPI | undefined>();
   const [searchResults, setSearchResults] = useState<APIResponse | undefined>();
   const [page, setPage] = useState<number>(1);
 
@@ -19,13 +19,17 @@ export const Search:React.FC<ChildProps> = ({ query, config }) => {
   async function fetchAPI () {
     let search = await axios.get<APIResponse>(`http://localhost:8080/tmdb/search/${query}/${page}`);
     setSearchResults(search.data);
+    let config = await axios.get<ConfigAPI>(`http://localhost:8080/tmdb/configuration`);
+    setConfig(config.data);
   }
+
+  // handleChange(e: ChangeEvent<HTMLInputElement>)
 
   return (
       <>
         <h3>SEARCH RESULTS</h3>
-        {/* {topTV !== undefined ?
-        <YourWatchList watchList={topTV.results} config={config}/>: null} */}
+        {searchResults !== undefined ?
+        <YourWatchList watchList={searchResults.results} config={config}/>: null}
       </>
   );
 }
